4e39cdd268e802c22e90756797c69cb412c40649
Joallisson <joallissonnc@gmail.com>
2023-03-17 20:23:06 -0300

    criado animação e script de pular do player

--- Google Dinosaur Game/Assets/_Game/Scripts/Player.cs
+++ Google Dinosaur Game/Assets/_Game/Scripts/Player.cs
@@ -4,15 +4,81 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    public int speedJumping;
+    public float counterJump;
+    private float totalCounterJump;
+    private bool isJumping, isGrounding, canJump;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        totalCounterJump = counterJump;
+        isJumping = false;
+        isGrounding = true;
+        canJump = true;
+    }
+
+    private void FixedUpdate()
+    {
+        Jumping();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        TouchScreen();
     }
+
+    private void OnCollisionEnter2D(Collision2D target)
+    {
+        if(target.gameObject.CompareTag("Ground"))
+        {
+            canJump = true;
+            isGrounding = true;
+            counterJump = totalCounterJump;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D target)
+    {
+        if (target.gameObject.CompareTag("Ground"))
+        {
+            isGrounding = false;
+        }
+    }
+
+    private void TouchScreen()
+    {
+
+        if(Input.touchCount > 0 && canJump)
+        {
+            counterJump -= Time.deltaTime;
+
+            if(counterJump > 0)
+            {
+                isJumping = true;
+            }
+            else
+            {
+                canJump = false;
+            }
+           
+        }
+        else if(!isGrounding)
+        {
+            canJump = false;
+            isJumping = false;
+        }
+    }
+
+    private void Jumping()
+    {
+        if (isJumping)
+        {
+            rb.velocity = Vector2.up * speedJumping;
+        }
+    }
+    
 }
