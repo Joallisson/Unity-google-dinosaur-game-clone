66609e18cd1200f6145281da307ee85a77cc8918
Joallisson <joallissonnc@gmail.com>
2023-08-16 19:39:22 -0300

    Refazendo jogo

---
+++ Google Dinosaur Game/Assets/_Game/Scripts/Backend.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Backend : MonoBehaviour
+{
+    private MeshRenderer meshRenderer;
+    [SerializeField] private float speedTexture;
+    void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    void Update()
+    {
+        Paralax();
+    }
+
+    private void Paralax()
+    {
+        meshRenderer.material.mainTextureOffset += new Vector2(speedTexture * Time.deltaTime, 0);
+    }
+}
